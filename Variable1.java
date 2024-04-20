30533e094fa6879543646cf9e0b37a033db3a72a
Third543 <thee0987@hotmail.com>
2024-04-20 17:45:51 +0700

    4/20/2567
    ก็นะเรียนเกี่ยวกับการนิยามตัวแปร

---
+++ Variable1.java
@@ -0,0 +1,19 @@
+class Variable {
+    public static void main(String[] args) {
+        //นิยามตัวแปรแบบไม่ระบุค่าเริ่มต้น
+        int number1;
+        boolean status;
+        char alphabet;
+        float number2;
+        double number3;
+
+        //นิยามตัวแปรแบบระบุค่าเริ่มต้น
+        int number=15;
+        float count=25.7f;
+        char as ='A';
+        boolean s =true;
+
+        //หลายบรรทัดในครั้งเดียว
+        int a=1 , b=2 , c=3;
+    }
+}
---
+++ Varible.java
@@ -0,0 +1,8 @@
+class Varible {
+    public static void main(String[] args) {
+       int num=10;
+       int num1=20;
+       System.err.println("ค่าตัวเลขที่ 1 = "+num);
+       System.err.println("ค่าตัวเลขที่ 2 = "+num1); 
+    }
+}
